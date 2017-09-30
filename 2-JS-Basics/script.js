10bf4751cc7ecb7791fb0c3b90e6c8349963726d
Sudarshan Venkataraman <sv79@buffalo.edu>
2017-09-30 19:52:47 -0400

    Different data types - Dynamic Typing

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,11 @@
 console.log('Javascript non Inline log');
+var name = "SUDIE";
+console.log(name);
+
+var lastName = "DOODIE";
+console.log(lastName);
+
+var age = 22;
+console.log(age);
+
+console.log(name+" "+lastName+" "+age);
