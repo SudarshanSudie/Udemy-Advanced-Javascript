45d2c6a9d317356ffab572f1fdbf5131803dd2eb
Sudarshan Venkataraman <sv79@buffalo.edu>
2017-10-01 12:24:15 -0400

    unshift shift pop push in JS arrays

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -191,3 +191,23 @@ var x = 3;
 if (x === 5) {
     //do soemthing
 }
+
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
+
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+console.log(john);
+
+if (john.indexOf('teacher') === -1) {
+    console.log('John is NOT a teacher.');
+}
