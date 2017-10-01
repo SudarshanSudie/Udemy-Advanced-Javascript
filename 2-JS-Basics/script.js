4d99752b3136ba8e640495557724073db21d1042
Sudarshan Venkataraman <sv79@buffalo.edu>
2017-09-30 22:14:24 -0400

    Operator precedence

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -36,3 +36,26 @@ var lastName = prompt('What is the last name?');
 //console.log(lastName);
 
 alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.' +' last name is: '+lastName);
+
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+//2016 - 52
+//1964
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+//ageJohn = ageMark = 26
+//ageJohn = 26
+
+ageJohn++;
+ageMark *= 2;
+ageMark = ageMark * 2;
+
+console.log(ageJohn);
+console.log(ageMark);
