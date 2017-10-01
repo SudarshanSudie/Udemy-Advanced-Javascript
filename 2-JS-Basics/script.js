e063e33ac7c8311652aec6a280e877d39042ab20
Sudarshan Venkataraman <sv79@buffalo.edu>
2017-09-30 20:47:24 -0400

    Type coercion and variable mutation

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -9,3 +9,30 @@ var age = 22;
 console.log(age);
 
 console.log(name+" "+lastName+" "+age);
+
+
+var name = 'Sudie';
+var age = 26;
+
+console.log(name + age);
+console.log(age + age);
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+
+var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.' +' last name is: '+lastName);
